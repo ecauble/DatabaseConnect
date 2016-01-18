66a23de17d236c417e03380fa4677b939be90ad1
Eric Cauble <caublee@email.uscupstate.edu>
2016-01-18 13:31:47 -0500

    initial commit

---
+++ DatabaseConnect/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  DatabaseConnect
+//
+//  Created by Eric Cauble on 1/17/16.
+//  Copyright © 2016 Eric Cauble. All rights reserved.
+//
+
+import Foundation
+
+//set this to localhost's ip
+let kLocalHost : String = "10.0.1.26"
---
+++ DatabaseConnect/ReigisterViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  DatabaseConnect
+//
+//  Created by Eric Cauble on 1/17/16.
+//  Copyright © 2016 Eric Cauble. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    
+    
+    //outlets
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let myUrl = NSURL(string: "http://localhost:8888/ghstest/registerClient.php")
+        
+        let request = NSMutableURLRequest(URL:myUrl!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "registerString=\(userName.text!)"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data:NSData?, response: NSURLResponse?, error:NSError?) -> Void in
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                if error != nil
+                {
+                    // display an alert message
+                    print(error!.localizedDescription)
+                    return
+                }
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                    
+                    if let parseJSON = json {
+                        if let objects  = parseJSON["registerString"] as? [AnyObject]
+                        {
+                            for obj in objects
+                            {
+                                let temp = (obj["registerString"] as! String)
+                                print(temp)
+                                // self.searchResults.append(fullName)
+                            }
+                            
+                        } else if(parseJSON["message"] != nil)
+                        {
+                            let errorMessage = parseJSON["message"] as? String
+                            if(errorMessage != nil)
+                            {
+                                // display an alert message
+                                
+                            }
+                        }
+                    }
+                    
+                } catch {
+                    print(error);
+                }
+            }
+        })
+        task.resume()
+
+    }
+
+}
+
